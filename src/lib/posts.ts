19acf4f76d792e6b919014d62d3a0599a423ef6a
Volva <volvazun@gmail.com>
2025-12-12 05:11:14 -0500

    Fixed issue of missing lib dir and its files

---
+++ src/lib/posts.ts
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'content/posts');
+
+export type PostData = {
+  id: string;
+  date: string;
+  title: string;
+  description?: string;
+  tags?: string[];
+};
+
+export function getSortedPostsData() {
+  // Create directory if it doesn't exist (prevents crash on fresh clone)
+  if (!fs.existsSync(postsDirectory)) {
+    fs.mkdirSync(postsDirectory, { recursive: true });
+  }
+
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".mdx" from file name to get id
+    const id = fileName.replace(/\.mdx$/, '');
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    return {
+      id,
+      ...matterResult.data,
+    } as PostData;
+  });
+
+  // Sort posts by date
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
